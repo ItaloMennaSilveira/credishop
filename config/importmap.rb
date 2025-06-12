56ff335ec66dcc12e2aa40b8cf2335dcb07fd68f
Italo Menna Silveira <italo.silveira@friday.ai>
2025-06-12 16:34:03 -0300

    feat: add rails-ujs

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,3 +8,4 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap" # @5.3.6
 pin "popper", to: "popper.js", preload: true
 pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
+pin "@rails/ujs", to: "rails-ujs.js"
