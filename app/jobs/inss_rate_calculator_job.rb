59fb317d8556794ba8a37c8d7db56335d36bd62e
Italo Menna Silveira <italo.silveira@friday.ai>
2025-06-14 17:42:02 -0300

    feat: remove inss rate job

--- app/jobs/inss_rate_calculator_job.rb
+++
@@ -1,8 +0,0 @@
-class InssRateCalculatorJob < ApplicationJob
-  queue_as :default
-
-  def perform(proponent_id, salary)
-    result = InssRateCalculatorService.new(salary).calculate
-    InssRateBroadcastService.new(proponent_id, result).broadcast
-  end
-end
