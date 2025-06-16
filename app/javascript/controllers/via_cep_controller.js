17d4e60dbc9be8c45967e835d89fbb57bf3ac923
Italo Menna Silveira <italo.silveira@friday.ai>
2025-06-15 23:03:17 -0300

    fix: number attr to mask

--- app/javascript/controllers/via_cep_controller.js
+++ app/javascript/controllers/via_cep_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["zip", "street", "city", "state", "number"]
+  static targets = ["zip", "street", "city", "state"]
 
   connect() {
     if (this.hasZipTarget) {
